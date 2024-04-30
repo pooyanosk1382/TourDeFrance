3ca0c9250dbc68f5687695c5d58252a1d4cc4d35
Pooyan <117625732+pooyanosk1382@users.noreply.github.com>
2024-04-30 19:43:16 +0330

    choosing country and speed is complete

--- main.py
+++ main.py
@@ -4,21 +4,9 @@ import random
 countries = list(pycountry.countries)
 random_numbers = [random.randint(0, len(countries)) for _ in range(8)]
 speed = [round(random.uniform(0.2, 0.5), 2) for _ in range(8)]
+country = [''] * 8
+for i in range(8):
+    country[i] = countries[random_numbers[i]].name
 
-country1 = countries[random_numbers[0]].name
-country2 = countries[random_numbers[1]].name
-country3 = countries[random_numbers[2]].name
-country4 = countries[random_numbers[3]].name
-country5 = countries[random_numbers[4]].name
-country6 = countries[random_numbers[5]].name
-country7 = countries[random_numbers[6]].name
-country8 = countries[random_numbers[7]].name
-
-print(country1, speed[0])
-print(country2, speed[1])
-print(country3, speed[2])
-print(country4, speed[3])
-print(country5, speed[4])
-print(country6, speed[5])
-print(country7, speed[6])
-print(country8, speed[7])
+for i in range(8):
+    print(country[i], speed[i])

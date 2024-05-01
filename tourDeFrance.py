7c7bb03e8eb424accc381c5421cc9352c51a6c08
Pooyan <117625732+pooyanosk1382@users.noreply.github.com>
2024-05-01 13:19:11 +0330

    debugging

--- tourDeFrance.py
+++ tourDeFrance.py
@@ -32,27 +32,10 @@ if __name__ == '__main__':
         country[i] = countries[random_numbers[i]].name
     for i in range(8):
         print(country[i], speed[i])
-    country1 = multiprocessing.Process(target=tournament, args=(country[0], speed[0]))
-    country2 = multiprocessing.Process(target=tournament, args=(country[1], speed[1]))
-    country3 = multiprocessing.Process(target=tournament, args=(country[2], speed[2]))
-    country4 = multiprocessing.Process(target=tournament, args=(country[3], speed[3]))
-    country5 = multiprocessing.Process(target=tournament, args=(country[4], speed[4]))
-    country6 = multiprocessing.Process(target=tournament, args=(country[5], speed[5]))
-    country7 = multiprocessing.Process(target=tournament, args=(country[6], speed[6]))
-    country8 = multiprocessing.Process(target=tournament, args=(country[7], speed[7]))
-    country1.start()
-    country2.start()
-    country3.start()
-    country4.start()
-    country5.start()
-    country6.start()
-    country7.start()
-    country8.start()
-    country1.join()
-    country2.join()
-    country3.join()
-    country4.join()
-    country5.join()
-    country6.join()
-    country7.join()
-    country8.join()
+    cyclist = [None] * 8
+    for i in range(8):
+        cyclist[i] = multiprocessing.Process(target=tournament, args=(country[i], speed[i]))
+    for i in range(8):
+        cyclist[i].start()
+    for i in range(8):
+        cyclist[i].join()

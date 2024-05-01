968c1874af9e55dbeda83fd7d205b7c32c8cae27
Pooyan <117625732+pooyanosk1382@users.noreply.github.com>
2024-05-01 13:55:44 +0330

    keep going

--- tourDeFrance.py
+++ tourDeFrance.py
@@ -16,11 +16,11 @@ def race(countryName, countrySpeed, day):
     return period
 
 
-def tournament(countryName, countrySpeed):
-    competitionTime = 0
-    for i in range(10):
-        competitionTime += race(countryName, countrySpeed, i)
-    print(str(countryName) + ' covered all distance in ' + str(competitionTime) + 'seconds.')
+def tournament(countryName, countrySpeed, competitionTime):
+    for i in range(2):
+        competitionTime.value += race(countryName, countrySpeed, i)
+    print(str(countryName) + ' covered all distance in ' + str(competitionTime.value) + 'seconds.')
+    return competitionTime
 
 
 if __name__ == '__main__':
@@ -28,14 +28,17 @@ if __name__ == '__main__':
     random_numbers = [random.randint(0, len(countries) - 1) for _ in range(8)]
     speed = [round(random.uniform(0.4, 0.5), 2) for _ in range(8)]
     country = [''] * 8
-    for i in range(8):
-        country[i] = countries[random_numbers[i]].name
-    for i in range(8):
-        print(country[i], speed[i])
     cyclist = [None] * 8
+    competitionTimes = [None] * 8
     for i in range(8):
-        cyclist[i] = multiprocessing.Process(target=tournament, args=(country[i], speed[i]))
-    for i in range(8):
+        competitionTimes[i] = multiprocessing.Value('d', 0.0)
+    for i in range(8):  # names of countries in the competition
+        country[i] = countries[random_numbers[i]].name
+    for i in range(8):  # print countries and their speed
+        print(country[i], speed[i])
+    for i in range(8):  # creating processes with target function of tournament
+        cyclist[i] = multiprocessing.Process(target=tournament, args=(country[i], speed[i], competitionTimes[i]))
+    for i in range(8):  # start processes
         cyclist[i].start()
-    for i in range(8):
+    for i in range(8):  # join processes
         cyclist[i].join()
